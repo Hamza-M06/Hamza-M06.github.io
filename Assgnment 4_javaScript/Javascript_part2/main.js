b9e53c696e0790da8a7c64d694897e7d63c22b0a
Hamza-M06 <hwmohamed7@gmail.com>
2025-08-01 00:03:52 -0400

    Commit 3 - image gallary
    Updated comments,   Declared the array of image filenames and  alternative text for each image file

--- Assgnment 4_javaScript/Javascript_part2/main.js
+++ Assgnment 4_javaScript/Javascript_part2/main.js
@@ -1,9 +1,11 @@
 /**
  * Name: Hamza Mohamed
- * File: Assignment 4 Part 1
- * Description:part 1 story generator using javascript
+ * File: Assignment 4 Part 2
+ * Description:Image gallary part 2 Assignment 4 
  * Date:July 2025
  */
+
+
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -11,10 +13,20 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const images = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"];
 /* Declaring the alternative text for each image file */
+const imgAlts = [
+  {"pic1": "Closeup of a blue human eye"},
+  {"pic2": "A Rock in the shape of a wave"}, 
+  {"pic3": "White and purple pansies"}, 
+  {"pic4": "section of a wall from pharoah's tomb"}, 
+  {"pic5": "closeup of moth on a leaf"}
 
+];
 /* Looping through images */
+
+
+
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
