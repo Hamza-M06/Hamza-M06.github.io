b7eb609e12ea8f6fa7d084113dc81b537adcf9fa
Hamza-M06 <hwmohamed7@gmail.com>
2025-07-31 23:22:09 -0400

    Commit 4 - silly story
    Added logic to replace placeholders in the story with random values. Implemented custom name substitution and UK unit conversion for temperature and weight. Finalized story output

--- Assgnment 4_javaScript/Javascript_part1/Silly.js
+++ Assgnment 4_javaScript/Javascript_part1/Silly.js
@@ -5,7 +5,6 @@
  * Date:July 2025
  */
 
-
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
@@ -31,17 +30,26 @@ function result() {
     const yItem = randomValueFromArray(insertY);
     const zItem = randomValueFromArray(insertZ);
 
-  if(customName.value !== '') {
-    const name = customName.value;
 
-  }
+    newStory = newStory
+        .replaceAll(":insertx:", xItem)
+        .replaceAll(":inserty:", yItem)
+        .replaceAll(":insertz:", zItem);
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
 
-  }
 
-  story.textContent = ;
+    if(customName.value !== '') {
+        const name = customName.value;
+        newStory = newStory.replaceAll('Bob', name);
+    }     
+
+    if(document.getElementById("uk").checked) {
+        const weight = `${Math.round(300*0.0714286)} stone`;
+        const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+        newStory = newStory.replaceAll('94 fahrenheit', temperature);
+        newStory = newStory.replaceAll('300 pounds', weight);
+    }
+
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
