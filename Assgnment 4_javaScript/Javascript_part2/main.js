5a43524f31e918d05a1c979c1ee5b6ead5c05930
Hamza-M06 <hwmohamed7@gmail.com>
2025-07-31 23:46:19 -0400

    Commit 2
    added comments

--- Assgnment 4_javaScript/Javascript_part2/main.js
+++ Assgnment 4_javaScript/Javascript_part2/main.js
@@ -1,3 +1,9 @@
+/**
+ * Name: Hamza Mohamed
+ * File: Assignment 4 Part 1
+ * Description:part 1 story generator using javascript
+ * Date:July 2025
+ */
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
